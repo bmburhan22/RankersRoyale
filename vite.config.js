c433408d54c85b9ceab24eb342c00d5b85247b35
Burhanuddin <bm.burhan22@gmail.com>
2024-10-07 15:54:55 +0530

    dynamic API_URL

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from  'dotenv';
+dotenv.config();
+const {DISCORD_OAUTH2_URL} = process.env;
+const { redirect_uri } = Object.fromEntries(new URL(DISCORD_OAUTH2_URL).searchParams);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{port:3000},
   plugins: [react()],
   define: {
-    API_URL: JSON.stringify('https://moray-organic-robin.ngrok-free.app')
+    API_URL: JSON.stringify(new URL(redirect_uri).origin)
   }
 })
