a9e513dac5288e38aeeb07f96716e3f6fc4c9dd1
Burhanuddin <bm.burhan22@gmail.com>
2024-10-07 19:46:54 +0530

    print API_URL

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,11 @@ import dotenv from  'dotenv';
 dotenv.config();
 const {DISCORD_OAUTH2_URL} = process.env;
 const { redirect_uri } = Object.fromEntries(new URL(DISCORD_OAUTH2_URL).searchParams);
-
+const  API_URL= JSON.stringify(new URL(redirect_uri).origin);
+console.log({API_URL})
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{port:3000},
   plugins: [react()],
-  define: {
-    API_URL: JSON.stringify(new URL(redirect_uri).origin)
-  }
+  define: {API_URL}
 })
